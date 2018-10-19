ccf8306f3a251e1103cc843195222ae18b5223ae
Gabriel Querbes <gabrielquerbes@abas-usa.com>
2018-10-19 19:50:16 -0400

    Add project files.

---
+++ TimeTracker/TimeTracker/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TimeTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel _vm;
+        public MainPage()
+        {
+            _vm = new MainPageViewModel();
+            this.BindingContext = _vm;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ TimeTracker/TimeTracker/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTracker
+{
+    public class MainPageViewModel
+    {
+        public List<List<string>> Entries => new List<List<string>>(){new List<string>{"Hello", "goodbye"}, new List<string>{"text","more"}};
+    }
+}
